39451045bb6c52e064c0e483b0acf7e9c71e2506
EKBEHTOP <43491787+EKBEHTOP@users.noreply.github.com>
2018-09-28 21:16:24 +0700

    И еще раз...

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
 
@@ -8,23 +8,32 @@
 bool date_correctness(int day, int month, int year) {
 	if (year > 0)
 	{
-		switch (mouth)
+		switch (month)
 		{
-		case 1, 3, 5, 7, 8, 10, 12:
-			if (0 < day < 32) return true;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			if (day > 0 && day < 32) return true;
 			else {
 				return false;
 			}
 			break;
-		case 4, 6, 9, 11:
-			if (0 < day < 31) return true;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			if (day > 0 && day < 31) return true;
 			else {
 				return false;
 			}
 			break;
 		case 2:
 			if (day == 29 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) return true;
-			else if (0 < day < 29) {
+			else if (day > 0 && day < 29) {
 				return true;
 			}
 			else
@@ -45,23 +54,23 @@ bool date_correctness(int day, int month, int year) {
 
 
 void test_date_correctness() {
-    assert(date_correctness(1,1,2018));
-    assert(date_correctness(30,5,1999));
-    assert(date_correctness(31,12,100));
-    assert(date_correctness(31,8,2016));
-    assert(date_correctness(29,2,2400));
-    assert(date_correctness(31,4,3000));
-    assert(!date_correctness(-1,3,1));
-    assert(!date_correctness(1,-3,1));
-    assert(!date_correctness(1,3,-1));
-    assert(!date_correctness(2,13,2001));
-    assert(!date_correctness(40,5,777));
-    assert(!date_correctness(29,2,1900));
+	assert(date_correctness(1, 1, 2018));
+	assert(date_correctness(30, 5, 1999));
+	assert(date_correctness(31, 12, 100));
+	assert(date_correctness(31, 8, 2016));
+	assert(date_correctness(29, 2, 2400));
+	assert(!date_correctness(31, 4, 3000));
+	assert(!date_correctness(-1, 3, 1));
+	assert(!date_correctness(1, -3, 1));
+	assert(!date_correctness(1, 3, -1));
+	assert(!date_correctness(2, 13, 2001));
+	assert(!date_correctness(40, 5, 777));
+	assert(!date_correctness(29, 2, 1900));
 
-    printf("Тесты прошли успешно!\n");
+	printf("Тесты прошли успешно!\n");
 }
 
 int main() {
-    test_date_correctness();
-    return 0;
+	test_date_correctness();
+	return 0;
 }
